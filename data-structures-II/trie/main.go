dd085b99f82e76a1945c34f97a9815e7c6391c79
toliveiradev <tiagojavapro@gmail.com>
2024-01-31 19:21:55 -0300

    Trie - Data Structures in Golang Series - JamieDev channel

---
+++ data-structures-II/trie/main.go
@@ -0,0 +1,74 @@
+package main
+
+import "fmt"
+
+// AlphabetSize is the number of possible characters in the trie
+const AlphabetSize = 26
+
+// Node represents each node in the trie
+type Node struct {
+	children [AlphabetSize]*Node
+	isEnd    bool
+}
+
+// Trie represents a trie and has a pointer to the root node
+type Trie struct {
+	root *Node
+}
+
+// InitTrie will create new Trie
+func InitTrie() *Trie {
+	result := &Trie{root: &Node{}}
+	return result
+}
+
+// Insert
+func (t *Trie) Insert(w string) {
+	wordLength := len(w)
+	currentNode := t.root
+	for i := 0; i < wordLength; i++ {
+		charIndex := w[i] - 'a'
+		if currentNode.children[charIndex] == nil {
+			currentNode.children[charIndex] = &Node{}
+		}
+		currentNode = currentNode.children[charIndex]
+	}
+	currentNode.isEnd = true
+}
+
+// Search will take in a word and RETURN true if that word is included in the trie
+func (t *Trie) Search(w string) bool {
+	wordLenght := len(w)
+	currentNode := t.root
+	for i := 0; i < wordLenght; i++ {
+		charIndex := w[i] - 'a'
+		if currentNode.children[charIndex] == nil {
+			return false
+		}
+		currentNode = currentNode.children[charIndex]
+	}
+	if currentNode.isEnd {
+		return true
+	}
+	return false
+}
+
+func main() {
+	myTrie := InitTrie()
+
+	toAdd := []string{
+		"aragorn",
+		"aragon",
+		"argon",
+		"eragon",
+		"oregon",
+		"oregano",
+		"oreo",
+	}
+
+	for _, v := range toAdd {
+		myTrie.Insert(v)
+	}
+
+	fmt.Println(myTrie.Search("wizard"))
+}
